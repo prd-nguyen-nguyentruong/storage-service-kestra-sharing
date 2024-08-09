fe532a4d424ae2565e45b98ff27998d3c755b613
Nguyen Nguyen Truong <nguyen.nguyentruong@paradox.ai>
2024-08-09 15:31:45 +0700

    chore: update env

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
 import app from "./src/app.ts";
 import { createServer } from 'node:http';
+import { APP_PORT } from './src/config/app.config';
 
 const server = createServer(app);
 
-server.listen(3000, () => {
-  console.log(`Server is running on port ${3000}`);
+server.listen(APP_PORT, () => {
+  console.log(`Server is running on port ${APP_PORT}`);
 });
 
 export default app;
--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -6,3 +6,4 @@ dotenv.config();
 export const CLOUDINARY_NAME= process.env.CLOUDINARY_NAME || "";
 export const CLOUDINARY_API_KEY= process.env.CLOUDINARY_API_KEY || "";
 export const CLOUDINARY_API_SECRET= process.env.CLOUDINARY_API_SECRET || "";
+export const APP_PORT= process.env.APP_PORT || 3000;
